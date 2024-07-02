2a49866f90f2a26272d8da00f8400fbf40d65157
mtaalha <abutalha1297@gmail.com>
2024-07-02 22:51:52 +0600

    first my file upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready (function (){
+  $('.slider').slick({
+      slidesToShow:3,
+      slidesToScroll:1,
+      dots: true,
+      prevArrow: false,
+      nextArrow: false,
+      autoplay: true,
+  });
+  $('.slider-2').slick({
+      slidesToShow:4,
+      slidesToScroll:1,
+      dots: true,
+      prevArrow: false,
+      nextArrow: false,
+      autoplay: true,
+  });
+})
